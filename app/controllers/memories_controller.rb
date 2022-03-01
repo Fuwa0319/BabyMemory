f7c06fa251c6a6b532af63ba81d662251b3ab7b1
Satoshi0118 <fwstr0319@gmail.com>
2022-03-01 15:47:52 +0900

    memories_controller修正

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -4,14 +4,12 @@ class MemoriesController < ApplicationController
   def index
     if params[:'top-date-select'].present?
       select_date = params[:'top-date-select']
-      @memories = Memory.includes(:user).where(worked_at: select_date.in_time_zone.all_day).order("worked_at ASC")
+      @memories = Memory.includes(:user).where(user_id: current_user, worked_at: select_date.in_time_zone.all_day).order("worked_at ASC")
       @check_day = select_date
-      #binding.pry
     else
       @check_day = Date.today.strftime("%Y-%m-%d")
       select_date = @check_day
-      @memories = Memory.includes(:user).where(worked_at: select_date.in_time_zone.all_day).order("worked_at ASC")
-      #binding.pry
+      @memories = Memory.where(user_id: current_user, worked_at: select_date.in_time_zone.all_day).includes(:user).order("worked_at ASC")
     end
   end
 
