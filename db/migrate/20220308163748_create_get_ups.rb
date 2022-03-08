1a83fb0946821d87529ffb56a9fe6fdfd1399448
Satoshi0118 <fwstr0319@gmail.com>
2022-03-09 01:42:06 +0900

    sleep/get_up migrateファイル作成

--- db/migrate/20220308163748_create_get_ups.rb
+++ db/migrate/20220308163748_create_get_ups.rb
@@ -1,7 +1,9 @@
 class CreateGetUps < ActiveRecord::Migration[6.0]
   def change
     create_table :get_ups do |t|
-
+      t.string  :memo
+      t.references  :user,   null: false, foreign_key: true
+      t.references  :memory, null: false, foreign_key: true
       t.timestamps
     end
   end
