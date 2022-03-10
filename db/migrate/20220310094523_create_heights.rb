00486b02c666cabb1551d17ee53ecd88ae8d8be6
Satoshi0118 <fwstr0319@gmail.com>
2022-03-10 18:52:52 +0900

    height マイグレーションファイル更新

--- db/migrate/20220310094523_create_heights.rb
+++ db/migrate/20220310094523_create_heights.rb
@@ -1,7 +1,10 @@
 class CreateHeights < ActiveRecord::Migration[6.0]
   def change
     create_table :heights do |t|
-
+      t.integer     :height_list_id,  null:false
+      t.string      :memo
+      t.references  :user, null: false, foreign_key: true
+      t.references  :memory, null: false, foreign_key: true
       t.timestamps
     end
   end
