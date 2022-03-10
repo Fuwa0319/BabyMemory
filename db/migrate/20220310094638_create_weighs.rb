d9eaf4dfdb8e703a864c28b358d63beb16018b5f
Satoshi0118 <fwstr0319@gmail.com>
2022-03-10 18:54:29 +0900

    weigh マイグレーションファイル更新

--- db/migrate/20220310094638_create_weighs.rb
+++ db/migrate/20220310094638_create_weighs.rb
@@ -1,7 +1,10 @@
 class CreateWeighs < ActiveRecord::Migration[6.0]
   def change
     create_table :weighs do |t|
-
+      t.integer     :weigh_list_id,  null:false
+      t.string      :memo
+      t.references  :user, null: false, foreign_key: true
+      t.references  :memory, null: false, foreign_key: true
       t.timestamps
     end
   end
